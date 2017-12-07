26ce29bbe70018e70afb804867231a8c8e60214f
Elliot Shin <wodshin@gmail.com>
2017-12-07 13:55:29 +0900

    testing cowboy websocket performance

---
+++ Makefile
@@ -0,0 +1,9 @@
+PROJECT = hello_erlang
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.1.0
+
+
+DEPS = cowboy
+dep_cowboy_commit = 2.1.0
+
+include erlang.mk
---
+++ src/ws_handler.erl
@@ -0,0 +1,26 @@
+-module(ws_handler).
+
+-export([init/2]).
+-export([websocket_init/1]).
+-export([websocket_handle/2]).
+-export([websocket_info/2]).
+
+init(Req, Opts) ->
+	{cowboy_websocket, Req, Opts}.
+
+websocket_init(State) ->
+%%	erlang:start_timer(1000, self(), <<"Hello!">>),
+	{ok, State}.
+
+websocket_handle({text, Msg}, State) ->
+	{reply, {text, Msg }, State};
+websocket_handle({binary, Msg}, State) ->
+	{reply, {binary, Msg }, State};
+websocket_handle(_Data, State) ->
+	{ok, State}.
+
+websocket_info({timeout, _Ref, Msg}, State) ->
+%%	erlang:start_timer(1000, self(), <<"How' you doin'?">>),
+	{reply, {text, Msg}, State};
+websocket_info(_Info, State) ->
+	{ok, State}.
